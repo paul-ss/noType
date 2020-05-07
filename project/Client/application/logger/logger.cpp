e59fee3b56fac0784b80b60f82aca59e662e1d7e
Muradyan Artem <mymailsoap@gmail.com>
2020-05-07 15:42:42 +0300

    add logger cpp

---
+++ project/Client/application/logger/logger.cpp
@@ -0,0 +1,18 @@
+#include "logger.hpp"
+
+void init_logger() {
+    boost::log::register_simple_formatter_factory<boost::log::trivial::severity_level, char>("Severity");
+
+    boost::log::add_file_log(
+        boost::log::keywords::file_name = "notype.log",
+        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
+    );
+
+    boost::log::core::get()->set_filter
+    (
+        boost::log::trivial::severity >= boost::log::trivial::info
+    );
+
+    boost::log::add_common_attributes();
+}
+
--- project/Client/application/logger/logger.hpp
+++ project/Client/application/logger/logger.hpp
@@ -6,18 +6,4 @@
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 
-void init_logger() {
-    boost::log::register_simple_formatter_factory<boost::log::trivial::severity_level, char>("Severity");
-
-    boost::log::add_file_log(
-        boost::log::keywords::file_name = "notype.log",
-        boost::log::keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-    );
-
-    boost::log::core::get()->set_filter
-    (
-        boost::log::trivial::severity >= boost::log::trivial::info
-    );
-
-    boost::log::add_common_attributes();
-}
+void init_logger();
