26ce3849cd68075eb3d4323282c9aeea2cfe9f7e
Pavel Smirnov <tratata@mail.ru>
2020-05-09 13:57:43 +0300

    Added BasicController interface

--- project/Server/source/Controller/BasicController.hpp
+++ project/Server/source/Controller/BasicController.hpp
@@ -4,9 +4,50 @@
 
 #pragma once
 
+#include "QueueManager.hpp"
+#include "DataBaseFacade.hpp"
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <memory>
+#include <thread>
+
+
+enum BasicControllerState {BASIC_CONTROLLER_RUN, BASIC_CONTROLLER_STOP};
+
 
 class BasicController {
+public:
 
+  BasicController(const std::shared_ptr<QueueManager> &queueManager,
+                 const std::shared_ptr<DataBaseFacade> &dataBaseFacade);
+  BasicController() = default;
+  ~BasicController() = default;
+  void startController();
+  void stopController();
+
+
+private:
+  void commandDistributor(const std::shared_ptr<Command> &command);
+  void startGameSessionHandler(const std::shared_ptr<Command> &command);
+  void getTextHandler(const std::shared_ptr<Command> &command);
+  void getRoomStatusHandler(const std::shared_ptr<Command> &command);
+  void sendWrittenTextHandler(const std::shared_ptr<Command> &command);
+
+  // void runClientsObserver(); ??
+  void runQueueWorker();
+
+
+private:
+  std::shared_ptr<QueueManager> _queueManager ;
+  std::shared_ptr<DataBaseFacade> _dataBaseFacade;
+
+
+//  boost::asio::io_service _service;
+//  boost::asio::io_service::work _work;
+  std::mutex _gameControllerMutex;
+  BasicControllerState _state;
+  std::vector<std::thread> _threads;
 };
 
 
