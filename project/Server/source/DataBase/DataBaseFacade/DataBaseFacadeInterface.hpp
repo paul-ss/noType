fd5c743b8257edd78e5324c2b44219b9239475d6
vendroid <vendroid@mail.ru>
2020-05-09 17:58:26 +0300

    Trying to Thread Safe DB

--- project/Server/source/DataBase/DataBaseFacade/DataBaseFacadeInterface.hpp
+++ project/Server/source/DataBase/DataBaseFacade/DataBaseFacadeInterface.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
-#include "DataBaseFacadeInterface.hpp"
-#include <thread>
+#include "TextInfoImpl.hpp"
+#include "PlayerInfoImpl.hpp"
 
-class DataBaseFacade : public IDataBaseFacade {
+#include <memory>
+
+class IDataBaseFacade {
 public:
-  DataBaseFacade();
-  DataBaseFacade(const std::string& dataBaseName);
-
 // NOTE(vendroid): Работа с таблицей информации о игроках
   void InsertPlayerInfo(std::unique_ptr<DataBase::External::PlayerInfo> plyerInfo);
   void UpadatePlayerInfo(std::unique_ptr<DataBase::External::PlayerInfo> plyerInfo);
@@ -16,10 +15,4 @@ public:
 
 // NOTE(vendroid): Работа с таблицей содержащих тексты
   std::unique_ptr<DataBase::External::TextInfo> GetRandomText();
-
-private:
-  std::mutex _mtx;
-  std::string _dataBaseName;
-  DataBase::External::PlayerInfoMapper _playerInfoMapper;
-  DataBase::External::TextInfoMapper _textInfoMapper;
 };
