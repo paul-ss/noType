d4fef4835fb785196be7ec36befa9cfba7efe29c
vendroid <vendroid@mail.ru>
2020-05-18 22:44:58 +0300

    added multithreding test

--- project/Client/application/Network/QueueManager/test_QueueManager.cpp
+++ project/Client/application/Network/QueueManager/test_QueueManager.cpp
@@ -1,10 +1,11 @@
-#include <gtest/gtest.h>
-
 #include "Message.hpp"
 #include "QueueManager.hpp"
 
+#include <gtest/gtest.h>
 
-TEST(QueueManager, pushToSendingData) {
+#include <thread>
+
+TEST(QueueManager, PushToSendingData) {
   auto queueManager
     = std::make_unique<Network::QueueManager>();
   {
@@ -19,20 +20,47 @@ TEST(QueueManager, pushToSendingData) {
   }
 }
 
-TEST(QueueManager, pushToSendingData) {
+TEST(QueueManager, PushToReceivedData) {
   auto queueManager
     = std::make_unique<Network::QueueManager>();
+
+  {
+    Network::InitResponse initRequest = {};
+    auto msg = std::make_unique<Network::Message>(Network::MessageType::InitRequest, initRequest);
+    queueManager->PushToReceivedData(std::move(msg));
+  }
+
+  {
+    auto msg = queueManager->PopReceivedData();
+    EXPECT_EQ(msg->GetMessageType(), Network::MessageType::InitRequest);
+  }
+}
+
+
+TEST(QueueManager, PushToSendingDataMultiThreads) {
+  auto queueManager
+    = std::make_unique<Network::QueueManager>();
+
+  std::thread thread([&]() {
+    while (true) {
+      if (auto msg = queueManager->PopSendingData()) {
+        EXPECT_EQ(msg->GetMessageType(), Network::MessageType::InitResponse);
+        break;
+      }
+      // NOTE(vendroid): Делаем какую-то несложную логику
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+  });
+
   {
     Network::InitResponse initResponse = {};
     auto msg = std::make_unique<Network::Message>(Network::MessageType::InitResponse, initResponse);
     queueManager->PushToSendingData(std::move(msg));
   }
 
-  {
-    auto msg = queueManager->PopSendingData();
-    EXPECT_EQ(msg->GetMessageType(), Network::MessageType::InitResponse);
-  }
+  thread.join();
 }
+
 
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
