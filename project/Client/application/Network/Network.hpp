03c5cd39e991f53f3d40ef3f373c50a568ea44b8
Pavel Smirnov <tratata@mail.ru>
2020-05-27 13:09:40 +0300

    Only for PR

--- project/Client/application/Network/Network.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Message.hpp"
-#include "MessageParser.hpp"
-#include "QueueManager.hpp"
-#include "NetworkManager.hpp"
--- project/Client/application/logger/logger.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-
-void init_logger();
