ec69a0fc3d3f5381afa183ccb75ad87a3fdcc031
vendroid <vendroid@mail.ru>
2020-05-31 14:08:27 +0300

    removed unnecessary files for Shukhtin pull request

--- project/Server/source/Logger/Logger.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-
-void initLogger(boost::log::trivial::severity_level l_severity);
